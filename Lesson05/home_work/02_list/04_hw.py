e51c122f3623a3f486bdda178e13a3411c5e797c
mech-1 <mech5579@gmail.com>
2025-04-29 01:38:52 +0200

    Update 04_hw.py

--- Lesson05/home_work/02_list/04_hw.py
+++ Lesson05/home_work/02_list/04_hw.py
@@ -5,3 +5,18 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+import math
+
+source_list = [2, -5, 8, 9, -25, 25, 4]
+new_list = []
+
+for number in source_list:
+    if number > 0:
+        new_item = number ** 0.5
+        # if new_item - int(new_item) == 0:
+        # if new_item % 1 == 0:
+        frac, intNum = math.modf(new_item)
+        if frac == 0:
+            new_list.append(int(new_item))
+
+print(new_list)
