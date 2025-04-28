8a8e48e02d5032bb2e6e90bf9a367c615e06f74b
mech-1 <mech5579@gmail.com>
2025-04-29 01:20:02 +0200

    Update 01_hw.py

--- Lesson05/home_work/02_list/01_hw.py
+++ Lesson05/home_work/02_list/01_hw.py
@@ -4,6 +4,18 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+# Variant 1
+for name in names:
+    print(f'{name}', end=", ")
 
+print()
+print('-' * 40)
+
+# Variant 2
+for i, name in enumerate(names, 1):
+    if i != len(names):
+        print(f'{name}', end=", ")
+    else:
+        print(f'{name}')
 # Пример вывода:
 # Иван, Ирина, Вячеслав, Василий, Петр
