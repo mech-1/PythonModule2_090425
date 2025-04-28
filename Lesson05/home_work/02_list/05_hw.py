bd04e2441d4d351bf6e06915f1df4c7a0e06ca97
mech-1 <mech5579@gmail.com>
2025-04-29 01:24:43 +0200

    Update 05_hw.py

--- Lesson05/home_work/02_list/05_hw.py
+++ Lesson05/home_work/02_list/05_hw.py
@@ -4,3 +4,12 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+i = 0
+longest_index = 0
+
+while i < len(names):
+    if len(names[longest_index]) < len(names[i]):
+        longest_index = i
+    i = i + 1
+
+print(names[longest_index])
