79ca7d200095daf1d7c7ddceeb6424b19b7de5b1
mech-1 <mech5579@gmail.com>
2025-05-02 01:38:52 +0200

    Update 02_hw_dict.py

--- Lesson07/home_work/02_hw_dict.py
+++ Lesson07/home_work/02_hw_dict.py
@@ -26,23 +26,23 @@ staff = [
         'salary': 47800
     },
 ]
-# Вычислите:
-print("Имя и Фамилию сотрудника с самой высокой зарплатой:")
 
-# TODO: your code here
+sorted_staff = sorted(staff, key=lambda x: x['salary'], reverse=False)
 
-print("Имя и Фамилию сотрудника с самой низкой зарплатой:")
+print(f"Имя и Фамилию сотрудника с самой высокой зарплатой: {sorted_staff[-1]['name']} {sorted_staff[-1]['surname']}")
 
-# TODO: your code here
+print(f"Имя и Фамилию сотрудника с самой низкой зарплатой: {sorted_staff[0]['name']} {sorted_staff[0]['surname']}")
 
-print("Среднеарифметическую зарплату всех сотрудников")
-
-# TODO: your code here
-
-print("Количество однофамильцев в организации")
-
-# TODO: your code here
-
+total_salary = 0
+surnames_set = set()
 print("*Список всех сотрудников(Имя и Фамилию) в порядке возрастания их зарплаты")
+for person in sorted_staff:
+    total_salary += person['salary']
+    surnames_set.add(person['surname'])
+    print(person['name'], person['surname'])
 
-# TODO: your code here
+avg_salary = total_salary / len(sorted_staff)
+print(f"Среднеарифметическую зарплату всех сотрудников: {avg_salary}")
+
+same_surname = len(staff) - len(surnames_set)
+print(f"Количество однофамильцев в организации: {same_surname} ")
