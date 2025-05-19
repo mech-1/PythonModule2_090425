726ec2df840192d7cdbaf9be6b720ddfd0a96f5d
mech-1 <mech5579@gmail.com>
2025-05-19 23:14:56 +0200

    Update 06_task_sort.py

--- Lesson18/home_work/06_task_sort.py
+++ Lesson18/home_work/06_task_sort.py
@@ -13,3 +13,26 @@
 # 70
 # Пояснение:
 # Возможен такой порядок: 10 2 50 1 10
+prices1 = [2, 1, 10, 50, 11, 5]
+prices2 = [2, 1, 10, 50, 12]
+
+
+def sum_max_prices(prices: list[int]) -> int:
+    sorted_prices = []
+    prices.sort()
+    j = len(prices)
+    # half_length = len(prices) // 2 if len(prices) % 2 == 0 else len(prices) // 2 + 1
+    half_length = ceil(len(prices) / 2)
+    for i in range(half_length):
+        j -= 1
+        sorted_prices.append(prices[j])
+        if i != j:
+            sorted_prices.append(prices[i])
+    return sum(sorted_prices[x] for x in range(0,len(sorted_prices),2))
+
+
+if __name__ == '__main__':
+    print(sum_max_prices(prices1))
+    print(sum_max_prices(prices2))
+    assert sum_max_prices(prices1) == 71
+    assert sum_max_prices(prices2) == 72
